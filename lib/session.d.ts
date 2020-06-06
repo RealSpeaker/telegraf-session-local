3159ebe70c08eb116505b6792c367b8137bd128f
Vladyslav Dalechyn <hotwater438@tutanota.com>
2020-06-06 04:37:55 +0300

    typescript: basic typings added (#59)
    * typescript: basic typings added
    
    * typescript: moved typing to lib
    
    * Update lib/session.d.ts
    
    Remove unexported isPromise
    
    * Update lib/session.d.ts
    
    LocalSessionOptions:format update
    
    * added state interface, applied suggested changes
    
    * removed LocalSesisonState interface
    
    * little refactoring
    
    * add @types/lowdb to dependencies
    
    * Update lib/session.d.ts
    
    Improved typings
    
    * use Lowdb.lowdb for DB property
    
    * Remove old lowdb getters
    
    * fix saveSession returning session object
    
    * fix double import from lowdb
    
    * type DB as unknown, minor fixes
    
    Co-authored-by: EdJoPaTo <github@edjopato.de>
    Co-authored-by: Tema Smirnov <github.tema@smirnov.one>

---
+++ lib/session.d.ts
@@ -0,0 +1,33 @@
+declare module 'telegraf-session-local' {
+  import { AdapterSync, AdapterAsync, BaseAdapter, lowdb } from 'lowdb'
+  import { ContextMessageUpdate, Middleware } from 'telegraf'
+
+  export interface LocalSessionOptions<TSession> {
+    storage?: AdapterSync | AdapterAsync
+    database?: string
+    property?: string
+    state?: TSession
+    format?: {
+        serialize?: (value: TSession) => string
+        deserialize?: (value: string) => TSession
+    }
+    getSessionKey?: (ctx: ContextMessageUpdate) => string
+  }
+
+  class LocalSession<TSession> {
+    public DB: unknown
+
+    constructor(options?: LocalSessionOptions<TSession>)
+
+    getSessionKey(ctx: ContextMessageUpdate): string
+    getSession(key: string): TSession
+    saveSession(key: string, data: TSession): Promise<TSession>
+    middleware(property?: string): Middleware<ContextMessageUpdate>
+    static get storageFileSync(): AdapterSync
+    static get storageFileAsync(): AdapterAsync
+    static get storageMemory(): AdapterSync
+    static get storageBase(): BaseAdapter
+  }
+
+  export default LocalSession
+}
