a5bfc959141528e14f470175574654289e01e5aa
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-01-21 10:55:04 +0100

    refactor: redefine MiddlewareFn to prevent breaking
    redefining something from a lib is bad but as the import changed it
    would be breaking otherwise

--- lib/session.d.ts
+++ lib/session.d.ts
@@ -1,6 +1,8 @@
 declare module 'telegraf-session-local' {
   import { AdapterSync, AdapterAsync, BaseAdapter } from 'lowdb'
-  import { Context, Middleware } from 'telegraf'
+  import { Context } from 'telegraf'
+
+  type MiddlewareFn<C extends Context> = (ctx: C, next: () => Promise<void>) => Promise<void>
 
   export interface LocalSessionOptions<TSession> {
     storage?: AdapterSync | AdapterAsync
@@ -22,7 +24,7 @@ declare module 'telegraf-session-local' {
     getSessionKey(ctx: Context): string
     getSession(key: string): TSession
     saveSession(key: string, data: TSession): Promise<TSession>
-    middleware(property?: string): Middleware<Context>
+    middleware(property?: string): MiddlewareFn<Context>
     static get storageFileSync(): AdapterSync
     static get storageFileAsync(): AdapterAsync
     static get storageMemory(): AdapterSync
